c82e80833697682957ee3e5de0a4c2298e063c12
bassclefstudio <bassclefstudio-dev@outlook.com>
2020-12-05 14:15:31 -0500

    Updated ConverterService to allow for implementations to add their own DI code.

--- BassClefStudio.Serialization/DI/ConverterService.cs
+++ BassClefStudio.Serialization/DI/ConverterService.cs
@@ -30,9 +30,16 @@ namespace BassClefStudio.Serialization.DI
                 var builder = new ContainerBuilder();
                 ConverterModule converterModule = new ConverterModule(assemblies);
                 builder.RegisterModule(converterModule);
+                ConfigureServices(builder);
                 ConverterContainer = builder.Build();
             }
         }
+
+        /// <summary>
+        /// Configures any services that are not simply closed types of <see cref="IConverter{TFrom, TTo}"/> or known <see cref="IKeyManager{TItem, TStore}"/>s to the converter DI container.
+        /// </summary>
+        /// <param name="builder">The Autofac <see cref="ContainerBuilder"/>.</param>
+        protected virtual void ConfigureServices(ContainerBuilder builder) { }
 
         /// <summary>
         /// Serializes the <see cref="TItem"/> into a <see cref="TOutput"/>.
