84b7ffbdac0e84803aeb3727a34ed8ce35b2d291
bassclefstudio <bassclefstudio-dev@outlook.com>
2020-12-15 17:47:54 -0500

    v2.1.1 - includes default value-type serializers when initializing a SerializationService.

---
+++ BassClefStudio.NET.Serialization/CustomTypes/VectorSerializer.cs
@@ -0,0 +1,37 @@
+﻿using BassClefStudio.NET.Serialization.Graphs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+
+namespace BassClefStudio.NET.Serialization.CustomTypes
+{
+    /// <summary>
+    /// An <see cref="ICustomSerializer"/> for dealing with <see cref="Guid"/>s
+    /// </summary>
+    public class VectorSerializer : ICustomSerializer
+    {
+        /// <inheritdoc/>
+        public TypeGroup ApplicableTypes { get; } = new TypeGroup(typeof(Vector2));
+
+        /// <inheritdoc/>
+        public object Deserialize(string value)
+        {
+            var array = value.Split(new string[] { ";" }, StringSplitOptions.RemoveEmptyEntries);
+            if(array.Length != 2)
+            {
+                throw new GraphException("Vector2 serialization expects two float parameters.");
+            }
+            var fs = array.Select(c => float.Parse(c)).ToArray();
+            return new Vector2(fs[0], fs[1]);
+        }
+
+        /// <inheritdoc/>
+        public string Serialize(object o)
+        {
+            var v = (Vector2)o;
+            return $"{v.X};{v.Y}";
+        }
+    }
+}
